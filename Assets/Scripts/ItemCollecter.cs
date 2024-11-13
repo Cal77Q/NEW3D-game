0d1d8d663f2d75af1e7568424e7580490d821e5a
Cal77Q <cqi2024@elam.co.uk>
2024-11-13 12:43:32 +0000

    s

--- Assets/Scripts/ItemCollecter.cs
+++ Assets/Scripts/ItemCollecter.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ItemCollecter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    int coins = 0;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coins++;
+        }
     }
 }
