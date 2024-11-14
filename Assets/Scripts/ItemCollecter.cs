fc4045d8f45dbfd8567c0cdd3fbdb54930e53bec
Cal77Q <cqi2024@elam.co.uk>
2024-11-14 14:45:50 +0000

    SASASASASASAAS

--- Assets/Scripts/ItemCollecter.cs
+++ Assets/Scripts/ItemCollecter.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollecter : MonoBehaviour
 {
     int coins = 0;
+
+    [SerializeField] Text
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +15,7 @@ public class ItemCollecter : MonoBehaviour
         {
             Destroy(other.gameObject);
             coins++;
+            Debug.Log("Coin:" + coins);
         }
     }
 }
