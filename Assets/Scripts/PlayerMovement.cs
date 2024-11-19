a7f40eb1f83e78e4cbcd234dc6b9fcab9cd60428
Cal77Q <cqi2024@elam.co.uk>
2024-11-19 09:29:54 +0000

    efef

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,57 +5,59 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-   [SerializeField] float movementSpeed = 6f;
-   [SerializeField] float jumpforce = 5f;
 
-   [SerializeField] Transform groundCheck;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
 
     [SerializeField] AudioSource jumpSound;
 
+    [SerializeField] float mouseSensitivity = 1000f;
+    float xRotation = 0f;
+    [SerializeField] Transform playerCamera;
 
-    
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Handle camera rotation
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Check if grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, ground);
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpSound.Play();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Handle movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
-
-        if  (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            Jump();
-        }
-
+        Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput;
+        Vector3 targetVelocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);
+        rb.velocity = targetVelocity;
     }
-
-    void Jump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, jumpforce, rb.velocity.z);
-        jumpSound.Play();
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy Head"))
-        {
-            Destroy(collision.transform.parent.gameObject);
-            Jump();
-        }
-    }
-
-    bool IsGrounded()
-    {
-        return Physics.CheckSphere(groundCheck.position, .1f, ground);
-    }
-
-
 }
